03609c8870956ce1311c6f75833c4c2d982d63f8
valentin hernandez <131786802+valentinhernandez1@users.noreply.github.com>
2024-04-25 13:21:36 -0300

    Respaldo.sh

---
+++ Respaldo.sh
@@ -0,0 +1,6 @@
+origen="C:\Users\valentin\Documentos\SistemasOperativos\Origen" 
+respaldo="C:\Users\valentin\Documentos\SistemasOperativos\Respaldo" 
+
+mkdir -p respaldo 
+cp -r origen respaldo 
+echo "La copia ha sido realizada correctamente.
